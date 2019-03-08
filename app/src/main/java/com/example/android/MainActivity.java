b6b78ecd86e8212976269def4ed52e690a492ad8
Nidheesh Panchal <nilu.1999@yahoo.com>
2019-03-08 16:02:32 +0530

    first page added

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -3,15 +3,39 @@ package com.example.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+	EditText email_text;
+	EditText pass_text;
+	Button login_button;
+	Button register_button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Log.d("newapp","hello");
+		email_text=findViewById(R.id.email_text);
+		pass_text=findViewById(R.id.password_text);
+		login_button=findViewById(R.id.login_button);
+		register_button=findViewById(R.id.register_button);
+
+		login_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+			}
+		});
+		register_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+			}
+		});
 	}
 
 }
